1a4477572d8d703ae1161ae8fe6d6fb50275680a
kawan idrees <kawanedres211@gmail.com>
2022-03-02 12:22:47 +0300

    my profile is updated

--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ProfileDetails from "./ProfileDetails"
 import images from "../Components/Images"
-import ExploreItems from './ExploreItems'
 import MyProfileItems from './MyProfileItems'
+import { Router,Route, Outlet } from 'react-router-dom'
+import {ReactComponent as P1} from "../Assets/icons/profile1.svg"
+import {ReactComponent as P2} from "../Assets/icons/profile2.svg"
+import {ReactComponent as P3} from "../Assets/icons/profile3.svg"
+import {ReactComponent as P4} from "../Assets/icons/profile4.svg"
+
+
 
 
 const MyProfile = () => {
@@ -13,9 +19,20 @@ const MyProfile = () => {
       <ProfileDetails />
     </div>
     {/* Bottom */}
-    <div className='mt-10 border-t  border-mGray grid grid-cols-3 cursor-pointer'>
 
-     
+
+    <div className='mt-10 border-t  border-mGray  cursor-pointer '>
+
+      <div className=' w-full mx-auto  flex justify-evenly px-32'>
+      <p ><P1 className='inline' />Posts</p>
+      <p ><P2 className='inline' />Vedios</p>
+      <p ><P3 className='inline' />Saved</p>
+      <p ><P4 className='inline' />Tagged</p>
+
+
+
+      </div>
+     <div className='grid grid-cols-3'>
     <MyProfileItems img={images[0]} />
     <MyProfileItems img={images[1]} />
     <MyProfileItems img={images[2]} />
@@ -49,27 +66,8 @@ const MyProfile = () => {
     <MyProfileItems img={images[13]} />
     <MyProfileItems img={images[14]} />
     <MyProfileItems img={images[15]} />
-   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    </div>
 
     </div>
   </>
