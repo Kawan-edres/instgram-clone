cbf8494c99a3217e2e0c2b603c2d960831b678eb
kawan idrees <kawanedres211@gmail.com>
2022-03-03 15:22:24 +0300

    home page made up active route

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,30 +3,25 @@ import Notifications from "../Pages/Notifications";
 import Home from "../Pages/Home";
 import Inbox from "../Pages/Inbox";
 import Posts from "../Pages/Posts";
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate,useNavigate } from "react-router-dom";
 import MyProfile from "./MyProfile";
 import New from "./New";
-import ProfilePosts from "./ProfilePosts";
-import ProfileVideos from "./ProfileVideos";
+
 
 const Main = () => {
-  const location = useLocation();
+  let location = useLocation();
+  const home=useNavigate("/home");
 
   return (
     <main className="h-screen max-w-[1000px] mx-auto mt-16     ">
       <Routes location={location}>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/instgram-clone" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/inbox" element={<Inbox />} />
         <Route path="/create/posts" element={<Posts />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/notifications" element={<Notifications />} />
-        <Route path="/myprofile" element={<MyProfile />}>
-          <Route to="/posts" element={<ProfilePosts />} />
-          <Route to="/videos" />
-          <Route to="/saved" />
-          <Route to="/tagged" />
-        </Route>
+        <Route path="/myprofile" element={<MyProfile />} />
         <Route path="/new" element={<New />} />
       </Routes>
     </main>
